023cfb7d7625b15256fd1446dc18116781cb08ec
grezar <grezar.dev@gmail.com>
2018-12-12 22:21:26 +0900

    Display virtual hosts

--- lib/sakuvm/cli.rb
+++ lib/sakuvm/cli.rb
@@ -5,7 +5,9 @@ module Sakuvm
   class CLI
     def self.start
       params = ARGV.getopts('h:p:u:', 'host:', 'password:', 'user:', 'insecure')
-      puts params
+      client = Sakuvm::Client.new(params)
+      vms = client.virtual_hosts
+      Sakuvm::Table.display(vms)
     end
   end
 end
